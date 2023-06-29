41e8671f7f157f18c7080c5f15f118d00f6b5f01
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-06-29 17:29:19 +0200

    variazione route + creazione admin controller

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth')->prefix("admin")->name('admin.')->group(function () {
+Route::get("/dashboard", [DashboardController::class, "index"])->middleware(["auth", "verified"])->name("dashboard");
 
-    Route::get('/', [AdminController::class, "index"])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
